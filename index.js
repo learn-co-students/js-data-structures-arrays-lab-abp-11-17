d88cdb2d0ea47d92784f2e35fd8a7be813c9836e
Carolyn Yuan <carolynlyuan@gmail.com>
2017-11-11 02:41:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const drivers = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendDriver(name) {
+  drivers.push('Ralph');
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift('Bob');
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  const allDrivers = [...drivers, 'Broom'];
+  return allDrivers;
+}
+
+function prependDriver(name) {
+  const moreDrivers = ['Arnold', ...drivers];
+  return moreDrivers;
+}
+
+function removeLastDriver() {
+  const lessDrivers = drivers.slice(0, drivers.length - 1);
+  return lessDrivers;
+}
+
+function removeFirstDriver() {
+  const minusFirstDriver = drivers.slice(1);
+  return minusFirstDriver;
+}
