cf953f03fe92f01ef9da485a4b16e8e202f8b3fb
Kyle Bennett <kyle@bennentterprise.com>
2017-11-12 00:40:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+// Write your solution
+const drivers=["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name){
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name){
+  drivers.unshift(name);
+}
+function destructivelyRemoveLastDriver(){
+  drivers.pop();
+}
+function destructivelyRemoveFirstDriver(){
+  drivers.shift();
+}
+
+function appendDriver(name){
+  const newArray=drivers.slice();
+  newArray.push(name);
+  return newArray;
+}
+function prependDriver(name){
+  const newArray=drivers.slice();
+  newArray.unshift(name);
+  return newArray;
+}
+function removeLastDriver(){
+  const ndrivers= drivers.slice(0, drivers.length-1);
+  return ndrivers;
+}
+function removeFirstDriver(){
+  const ndrivers=drivers.slice(1,drivers.length);
+  return ndrivers;
+
+}
