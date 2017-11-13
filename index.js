fa865d07884642c4900c731822174cb8b0741f35
Sasha V <sashav4@gmail.com>
2017-11-13 02:16:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 // Write your solution here!
 const drivers = ["Milo", "Otis", "Garfield"]
-function drivers(){
-  append.drivers("Ralph")
+function destructivelyAppendDriver(name){
+  drivers.push(name);
 }
-
-
-
-//destructivelyAppendDriver(name)
-//appends a driver to the end of the drivers array ‣
-//ReferenceError: destructivelyAppendDriver is not defined
-//    at Context.<anonymous> (test/indexTest.js:23:9)
+function destructivelyPrependDriver(name){
+  drivers.unshift(name);
+}
+function destructivelyRemoveLastDriver(name){
+  drivers.pop(name);
+}
+function destructivelyRemoveFirstDriver(name){
+  drivers.shift(name);
+}
+function appendDriver(name){
+  const appendeddrivers = [...drivers, (name)]
+  return appendeddrivers;
+}
+function prependDriver(name){
+  const prependeddrivers = [(name), ...drivers]
+  return prependeddrivers;
+}
+function removeLastDriver(){
+  const noLastDriver = drivers.slice(0, drivers.length -1)
+  return noLastDriver;
+}
+function removeFirstDriver(){
+  const noFirstDriver = drivers.slice(1)
+  return noFirstDriver;
+}
