2aaad621d6e91dca600b1c82360844af3cbaf7a1
Cory West-Forbes <cwestforbes@gmail.com>
2017-11-14 00:44:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+  drivers.push('Ralph');
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift('Bob');
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  const addDriver = [...drivers, 'Broom'];
+  return addDriver;
+}
+
+function prependDriver(name) {
+  const addDriver2 = ['Arnold', ...drivers];
+  return addDriver2;
+}
+
+function removeLastDriver() {
+  const sliceDriverEnd = drivers.slice(0, -1);
+  return sliceDriverEnd;
+}
+
+function removeFirstDriver() {
+  const sliceDriverFirst = drivers.slice(1);
+  return sliceDriverFirst;
+}
