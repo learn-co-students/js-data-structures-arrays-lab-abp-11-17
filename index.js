01d88e21ccf4701dc3decb39d9774594748bb022
carlitos321 <ca.cabrera321@gmail.com>
2017-11-17 00:36:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver() {
+  drivers.push("Ralph");
+  console.log(drivers)
+}
+
+function destructivelyPrependDriver() {
+  drivers.unshift("Bob");
+  console.log(drivers)
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+  console.log(drivers)
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+  console.log(drivers)
+}
+
+function appendDriver() {
+  const newDrivers = [...drivers, "Broom"];
+  console.log(newDrivers)
+  console.log(drivers)
+  return newDrivers
+}
+
+function prependDriver() {
+  const newNewDrivers = ["Arnold", ...drivers];
+  console.log(newNewDrivers)
+  console.log(drivers)
+  return newNewDrivers
+}
+
+function removeLastDriver() {
+const buttlessDrivers = drivers.slice(0,2);
+return buttlessDrivers
+}
+
+function removeFirstDriver() {
+  const headlessDrivers = drivers.slice(1);
+  return headlessDrivers
+}
