db545b7696a50c336683aef7c4c35555e8ae336b
Dean Stigall <stigallrealestate@gmail.com>
2017-11-20 18:51:21 +0000

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -49,37 +49,5 @@ describe('index.js', function () {
         expect(drivers).to.have.ordered.members(["Otis", "Garfield"]).and.to.not.include('Milo');
       });
     });
-
-    describe('appendDriver(name)', function () {
-      it('appends a driver to the drivers array and returns a new array, leaving the drivers array unchanged', function () {
-        expect(appendDriver("Broom")).to.have.ordered.members(["Milo", "Otis", "Garfield", "Broom"]);
-
-        expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
-      });
-    });
-
-    describe('prependDriver(name)', function () {
-      it('prepends a driver to the drivers array and returns a new array, leaving the drivers array unchanged', function () {
-        expect(prependDriver("Arnold")).to.have.ordered.members(["Arnold", "Milo", "Otis", "Garfield"]);
-
-        expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
-      });
-    });
-
-    describe('removeLastDriver()', function () {
-      it('removes the last driver in the drivers array and returns a new array, leaving the drivers array unchanged', function () {
-        expect(removeLastDriver()).to.have.ordered.members(["Milo", "Otis"]);
-
-        expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
-      });
-    });
-
-    describe('removeFirstDriver()', function () {
-      it('removes the first driver from the drivers array and returns a new array, leaving the drivers array unchanged', function () {
-        expect(removeFirstDriver()).to.have.ordered.members(["Otis", "Garfield"]);
-
-        expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
-      });
-    });
   });
 });
